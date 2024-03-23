ec574cd950c8535850548ff15dc83e93be8cd6ff
hpuranik <harshapuranik1704@gmail.com>
2024-03-23 15:59:50 -0400

    Removed credentials check in backend

--- server/routes/authJWT.js
+++ server/routes/authJWT.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
-const validCredentials = require("../middleware/validCredentials");
 const registerController = require("../controllers/registerController");
 const loginController = require("../controllers/loginController");
 
-router.post("/register", validCredentials, registerController.handleRegister);
+router.post("/register", registerController.handleRegister);
 
 router.post("/login", loginController.handleLogin);
 
